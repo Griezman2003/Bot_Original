38e4c2513efc3a913f09eff98a02d8aece484062
Griezman2003 <garciagomezgamaliel@gmail.com>
2022-11-17 14:10:01 -0600

    cambiando de lenguage a php taza de exito baja

---
+++ Controllers/bot.php
@@ -0,0 +1,30 @@
+<?php
+$token = '5743894715:AAH8UWLxPsMF5v3A1GEqjTemts8nLZUev0I';
+$website = 'https://api.telegram.org/bot'.$token;
+
+$input = file_get_contents('php://input');
+$update = json_decode($input, TRUE);
+
+$chatId = $update['message']['chat']['id'];
+$message = $update['message']['text'];
+
+switch($message) {
+    case '/start':
+        $response = 'Me has iniciado';
+        sendMessage($chatId, $response);
+        break;
+    case '/info':
+        $response = 'Hola! Soy Griezmann';
+        sendMessage($chatId, $response);
+        break;
+    default:
+        $response = 'No te he entendido';
+        sendMessage($chatId, $response);
+        break;
+}
+
+function sendMessage($chatId, $response) {
+    $url = $GLOBALS['website'].'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&text='.urlencode($response);
+    file_get_contents($url);
+}
+?>
